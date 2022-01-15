a2a090532c821740db63a24a3d15578e3b316415
Gravedd <troperread@gmail.com>
2022-01-15 14:24:49 +0400

    bug fix with links

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,8 +3,8 @@
         <a href="/">sChat</a>
     </div>
     <div class="nav">
-        <a href="home"><div class="button">Диалоги</div></a>
-        <a href="search"><div class="button">Поиск</div></a>
-        <a href="logout"><div class="button">Выход</div></a>
+        <a href="/home"><div class="button">Диалоги</div></a>
+        <a href="/search"><div class="button">Поиск</div></a>
+        <a href="/logout"><div class="button">Выход</div></a>
     </div>
 </header>
